89a40021edf6ed0e155da68c095cb9c80627d047
Shihab Sarar Rafid <shrafid.532@gmail.com>
2023-01-20 10:06:45 +0600

    header responsive done

--- Client/src/Components/Shared/Header/Header.js
+++ Client/src/Components/Shared/Header/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../Button/Button";
 import logo1 from "./../../../Images/logo.png";
 import "./Header.css";
@@ -6,13 +6,17 @@ import { IonIcon } from "@ionic/react";
 import { BsFillMenuButtonWideFill } from "react-icons/bs";
 import { ImMenu } from "react-icons/im";
 const Header = () => {
+  const [isClicked, setClicked] = useState(false);
   return (
     <div className="sidebar">
       <div className="left-part">
         <img className="logoImage" src={logo1} alt="Logo" />
         <p> MealHishab</p>
       </div>
-      <div className="right-part">
+      <div
+        className="right-part"
+        style={{ visibility: `${isClicked ? "visible" : "hidden"}` }}
+      >
         <ul>
           <li>Home</li>
           <li>User Guide</li>
@@ -28,7 +32,10 @@ const Header = () => {
 
         <span className="extra-btn">
           <Button text="Log In"></Button>
-          <ImMenu className="menuIcon"></ImMenu>
+          <ImMenu
+            onClick={() => setClicked(!isClicked)}
+            className="menuIcon"
+          ></ImMenu>
         </span>
       </div>
     </div>
